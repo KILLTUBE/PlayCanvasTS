d67c603051872866f1744090476f615452b9ef8b
kungfooman <lama12345@gmail.com>
2019-01-17 02:43:18 +0100

    remove most of string.js and make it .ts

---
+++ src/core/string.ts
@@ -0,0 +1,11 @@
+/**
+ * @namespace
+ * @name pc.string
+ * @description Extended String API
+ */
+namespace pc.string {
+    export function format(...args: any[]) {
+        console.log(arguments);
+        return "";
+    }
+}
